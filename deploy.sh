fcee33e9851dd82cae0b81e83d624d9b2092231b
jaxg <jacklyn.gil@gmail.com>
2019-10-14 09:17:35 -0400

    New jax-blog Build (Mon Oct 14 09:17:35 EDT 2019)

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,20 @@
+cd ~/www/jax-blog/
+
 git add .
-git commit -m "Update (`date`)"
+git commit -m "New jax-blog Build (`date`)"
 git push
 
 echo -e "\033[0;32mDeploying new blog...\033[0m"
 
 echo -e "\033[0;32mDeleting old site...\033[0m"
-cd ~/www/jaxgil.github.io
+cd ~/www/jaxgil.github.io/
+
 find -not -path "./.git/*" -not -name ".git" | grep git
 find -not -path "./.git/*" -not -name ".git" -delete
 
-
 echo -e "\033[0;32mRunning hugo...\033[0m"
-cd ~/www/jax-blog
-hugo -d ~/www/jaxgil.github.io
+cd ~/www/jax-blog/
+hugo -d ~/www/jaxgil.github.io/
 
 echo -e "\033[0;32mChanging to blog directory...\033[0m"
 
@@ -20,10 +22,11 @@ echo -e "\033[0;32mCommit and push the new build...\033[0m"
 cd ~/www/jaxgil.github.io
 
 git add .
-git commit -am "New Blog Build (`date`)"
+git commit -am "New jaxgil.com Build (`date`)"
 git push
 
-#echo -e "\033[0;32mChange back to jaxgil.com...\033[0m"
+echo -e "\033[0;32mChange back to jaxgil.com...\033[0m"
 cd ../jax-blog
 
 echo -e "\033[0;32mDeploy complete.\033[0m"
+
